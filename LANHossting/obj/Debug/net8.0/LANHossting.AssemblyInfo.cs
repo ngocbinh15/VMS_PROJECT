cdfc5cfb6ab2788f3da2d7de3614a05aac81785f
ngocbinh15 <dtshenlong1901@gmail.com>
2026-03-08 01:33:11 +0700

    Update static web assets cache and modify admin styles for action cards
    - Updated the static web assets cache with a new hash.
    - Enhanced the CSS for admin action cards to include styles for the new .ap-card-phao class.
    - Changed the background color of .ap-card-phao .ap-btn-access to use --admin-primary instead of --admin-info.

--- LANHossting/obj/Debug/net8.0/LANHossting.AssemblyInfo.cs
+++ LANHossting/obj/Debug/net8.0/LANHossting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LANHossting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d78ed8a845ba79b97b2c87d1994df2cd9086e166")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c43b990d26e96824c103d161e9151bd41b0a58b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LANHossting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LANHossting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
